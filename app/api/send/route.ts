37c480a65b0f093f3643a57910a6fc2acf4bfae3
kaushalrijal <kaushalrijal091@gmail.com>
2025-12-22 21:19:42 +0545

    generate template for each contact

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,8 +1,17 @@
+import renderTemplate from "@/lib/template/render";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    console.log("yeehaw")
     const body = await req.json()
-    console.log(body)
+    const {contacts, htmlOutput, subject} = body
+    for(const contact of contacts){
+        const eachBody = renderTemplate(htmlOutput, contact);
+        const eachSubject = renderTemplate(subject, contact);
+
+        console.log("------")
+        console.log('SUBJECT: ', eachSubject)
+        console.log('BODY: ', eachBody)
+        console.log("------")
+    }
     return NextResponse.json({success: "success"})
 }
