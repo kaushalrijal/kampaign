b73a1989f2962e7863d43c54488f37a0348546d1
kaushalrijal <kaushalrijal091@gmail.com>
2025-12-22 21:53:57 +0545

    add smtp test

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,17 +1,35 @@
+import { getEnvSMTPConfig } from "@/lib/server/smtp-config";
 import renderTemplate from "@/lib/template/render";
 import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 export async function POST(req: Request) {
+    // get request body
     const body = await req.json()
-    const {contacts, htmlOutput, subject} = body
+    const {contacts, htmlOutput, subject,recipientHeader, attachments } = body
+
+    // get environment variables and test
+    const env = getEnvSMTPConfig();
+    if(!env){
+        return NextResponse.json({success: false, message:"SMTP NOT CONFIGURED!"})
+    }
+
+    // create transporter
+    
+    
+    
     for(const contact of contacts){
         const eachBody = renderTemplate(htmlOutput, contact);
         const eachSubject = renderTemplate(subject, contact);
 
         console.log("------")
+        console.log('TO: ', contact[recipientHeader])
         console.log('SUBJECT: ', eachSubject)
         console.log('BODY: ', eachBody)
+        console.log('FILES: ', attachments)
         console.log("------")
+
+        const sent = await 
     }
     return NextResponse.json({success: "success"})
 }
