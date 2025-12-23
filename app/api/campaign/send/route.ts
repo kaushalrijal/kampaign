37374361679bd74cd9d17f1bf2551ee461e898d7
kaushalrijal <kaushalrijal091@gmail.com>
2025-12-23 23:49:10 +0545

    parse form data in api route

--- app/api/campaign/send/route.ts
+++ app/api/campaign/send/route.ts
@@ -4,50 +4,62 @@ import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
 export async function POST(req: Request) {
-    // get request body
-    const body = await req.json()
-    const {contacts, htmlOutput, subject,recipientHeader, attachments } = body
+  // parse form data
+  const formData = await req.formData();
 
-    // get environment variables and test
-    const env = getEnvSMTPConfig();
-    if(!env){
-        return NextResponse.json({success: false, message:"SMTP NOT CONFIGURED!"})
-    }
+  const payloadRaw = formData.get("payload");
+  if (!payloadRaw || typeof payloadRaw !== "string") {
+    return NextResponse.json(
+      { success: false, message: "Invalid payload" },
+      { status: 400 }
+    );
+  }
 
-    // create transporter
-    const transporter = await nodemailer.createTransport({
-        host: env.HOST,
-        port: env.PORT,
-        secure: false, // true for 465, false for other ports
-        auth: {
-          user: env.USER,
-          pass: env.APP_PASSWORD,
-        },
-      });
-    
-    
-    for(const contact of contacts){
-        const eachBody = renderTemplate(htmlOutput, contact);
-        const eachSubject = renderTemplate(subject, contact);
+  const { contacts, htmlOutput, subject, recipientHeader } = JSON.parse(payloadRaw);
 
-        console.log("------")
-        console.log('TO: ', contact[recipientHeader])
-        console.log('SUBJECT: ', eachSubject)
-        console.log('BODY: ', eachBody)
-        console.log('FILES: ', attachments)
-        console.log("------")
+  const files = formData.getAll("attachments") as File[];
 
-        const sent = await transporter.sendMail({
-            from: env.USER, 
-            to: contact[recipientHeader],
-            subject: eachSubject,
-            html: eachBody
-        })
+  // get environment variables and test
+  const env = getEnvSMTPConfig();
+  if (!env) {
+    return NextResponse.json({
+      success: false,
+      message: "SMTP NOT CONFIGURED!",
+    }, {status: 400});
+  }
 
+//   // create transporter
+//   const transporter = await nodemailer.createTransport({
+//     host: env.HOST,
+//     port: env.PORT,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//       user: env.USER,
+//       pass: env.APP_PASSWORD,
+//     },
+//   });
 
-        console.log("Message Sent: ", sent.messageId);
-    }
+  for (const contact of contacts) {
+    const eachBody = renderTemplate(htmlOutput, contact);
+    const eachSubject = renderTemplate(subject, contact);
 
-    console.log(attachments)
-    return NextResponse.json({success: "success"})
+    console.log("------");
+    console.log("TO: ", contact[recipientHeader]);
+    console.log("SUBJECT: ", eachSubject);
+    console.log("BODY: ", eachBody);
+    console.log("FILES: ", files);
+    console.log("------");
+
+    // const sent = await transporter.sendMail({
+    //   from: env.USER,
+    //   to: contact[recipientHeader],
+    //   subject: eachSubject,
+    //   html: eachBody,
+    // });
+
+    // console.log("Message Sent: ", sent.messageId);
+  }
+
+  console.log(files);
+  return NextResponse.json({ success: "success" });
 }
